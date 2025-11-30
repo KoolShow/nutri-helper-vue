ed41fc275b9c3d278191c069b2dfa1f773333be3
KoolShow <51787949+KoolShow@users.noreply.github.com>
2025-12-01 02:11:41 +0800

    feat: add metadata

--- vite.config.ts
+++ vite.config.ts
@@ -9,9 +9,18 @@ export default defineConfig({
     monkey({
       entry: "src/main.ts",
       userscript: {
+        name: "Nutri Helper",
+        author: "KoolShow, Northsea_403",
+        description: "Enhance nutrimatic.org",
         icon: "https://vitejs.dev/logo.svg",
         namespace: "npm/vite-plugin-monkey",
         match: ["https://nutrimatic.org/*"],
+        downloadURL:
+          "https://github.com/KoolShow/nutri-helper-vue/releases/download/latest/nutri-helper.user.js",
+        updateURL:
+          "https://github.com/KoolShow/nutri-helper-vue/releases/download/latest/nutri-helper.user.js",
+        homepageURL: "https://github.com/KoolShow/nutri-helper-vue/",
+        supportURL: "https://github.com/KoolShow/nutri-helper-vue/issues",
       },
       build: {
         externalGlobals: {
